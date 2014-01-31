021507f111b396df8dafe00c02e409b53e89c049
Vierkantor <vierkantor@vierkantor.com>
2014-01-31 21:29:35 +0100

    + Autogroup services that stop at a certain station with ones that start at that station

--- OVTijden.com/massage.py
+++ OVTijden.com/massage.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
 import os;
 import re;
+import sys;
 
 from bs4 import BeautifulSoup;
+
+def Time(hr = 0, min = 0, sec = 0):
+	if hr < 2:
+		hr += 24;
+	return hr * 60 + min + sec / 60;
 
 def getRows(htmldoc):
     soup = BeautifulSoup(htmldoc);
@@ -13,15 +19,23 @@ def mergeOrder(service, order):
 		services[service] = [];
 	if order not in services[service]:
 		services[service].append(order);
+	
+	services[service].sort(key=lambda order: order[1]);
 
-def mergeService(train, service):
-	if train not in trains:
-		trains[train] = [];
-	if service not in trains[train]:
-		trains[train].append(service);
+# sorts the services by departure time
+def mergeService(serviceData):
+	# find the insertion point
+	index = 0;
+	for service in servicesByTime:
+		if service[0][1] > serviceData[0][1]:
+			break;
+		index += 1;
+	
+	# insert before the later service
+	servicesByTime.insert(index, serviceData);
 
-trains = {};
 services = {};
+servicesByTime = [];
 
 for fileName in os.listdir("."):
 	if not fileName.endswith(".txt"):
@@ -49,14 +63,20 @@ for fileName in os.listdir("."):
 		
 		try:
 			if havePlatforms:
-				mergeOrder(contents[0], (contents[1], stationName[0].upper() + stationName[1:], contents[5]));
+				#        train id     - time                                              - S                      tation name             - track nr   - train type
+				order = (contents[0], Time(int(contents[1][0:2]), int(contents[1][3:5])), stationName[0].upper() + stationName[1:] + "S" + contents[5], contents[9]);
 			else:
-				mergeOrder(contents[0], (contents[1], stationName[0].upper() + stationName[1:]));
-			mergeService(contents[9] + " " + contents[0], contents[0]);
-		except IndexError:
+				order = (contents[0], Time(int(contents[1][0:2]), int(contents[1][3:5])), stationName[0].upper() + stationName[1:], contents[9]);
+			mergeOrder(contents[0], order);
+		except IndexError as e:
 			print("Manual massaging needed:");
 			print("File: {}".format(fileName));
 			print(row);
+			print("Error:");
+			print(e);
+
+for service in services:
+	mergeService(services[service]);
 
 print("version: 2");
 
@@ -64,17 +84,49 @@ print("services:");
 for service in services:
 	print("\t{}:".format(service));
 	for order in services[service]:
-		if len(order) >= 3:
-			print("\t\t{}, {}S{}".format(order[0], order[1], order[2]));
-		else:
-			print("\t\t{}, {}".format(order[0], order[1]));
+		print("\t\t{}, {}, {}".format(int(order[1] // 60), int(order[1] % 60), order[2]));
 	print("\t:end");
 print(":end");
 
 print("trains:");
-for train in trains:
-	print("\t{}:".format(train));
-	for service in trains[train]:
-		print("\t\t{}".format(service));
+index = 0;
+for service in servicesByTime:
+	# skip used services
+	if service == None:
+		newIndex += 1;
+		continue;
+	
+	# add this service to the new schedule
+	servicesByTime[index] = None;
+	schedule = [service];
+	while True:
+		lastOrder = schedule[-1][-1];
+		newIndex = 0;
+		for newService in servicesByTime:
+			if newService == None:
+				newIndex += 1;
+				continue;
+			
+			firstOrder = newService[0];
+			
+			# if the new service starts later than this one in the same station with the same train
+			if firstOrder[1] > lastOrder[1] and firstOrder[2] == lastOrder[2] and firstOrder[3] == lastOrder[3]:
+				schedule.append(newService);
+				servicesByTime[newIndex] = None;
+				break;
+			
+			newIndex += 1;
+		else:
+			break;
+	
+	# save the data
+	print("\t{} {}:".format(schedule[0][0][3], index));
+	
+	for service in schedule:
+		print("\t\t{}".format(service[0][0]));
+	
 	print("\t:end");
+	sys.stdout.flush();
+	
+	index += 1;
 print(":end");
