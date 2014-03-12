2496ec7f0ee5d5f563fe846402b3e0f57ca9a9f5
Vierkantor <vierkantor@vierkantor.com>
2014-03-12 15:36:43 +0100

    Made a couple common parser functions

---
+++ Parser.py
@@ -0,0 +1,39 @@
+import re;
+
+class ParseError(Exception):
+	def __init__(self, message):
+		self.message = message;
+	
+	def __str__(self):
+		return self.message;
+
+# returns a string, starting from the first non-whitespace character
+def SkipWhitespace(text):
+	while text[0] == " " or text[0] == "\t" or text[0] == "\n":
+		text = text[1:];
+	return text;
+
+# checks that the text starts with the literal
+def MatchText(text, literal):
+	text = SkipWhitespace(text);
+	if text[:len(literal)] == literal:
+		return (text[len(literal):], literal);
+	raise ParseError("Expected {}, received {}".format(literal, text[:len(literal)]));
+
+# checks that the text starts with an integer
+intRegex = re.compile(r"\+?(\-?\d+)");
+def MatchInt(text):
+	text = SkipWhitespace(text);
+	match = intRegex.match(text);
+	if match != None:
+		return (text[match.end(1):], int(match.group(1)));
+	raise ParseError("Expected <int>, received {}".format(text[:16]));
+
+# checks that the text starts with a float (or integer)
+floatRegex = re.compile(r"\+?(\-?\d+(.\d*)?)");
+def MatchFloat(text);
+	text = SkipWhitespace(text);
+	match = floatRegex.match(text);
+	if match != None:
+		return (text[match.end(1):], float(match.group(1)));
+	raise ParseError("Expected <float>, received {}".format(text[:16]));
