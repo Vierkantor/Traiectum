9829f63796b3c964bbdd1860ba52ab260fa8e59c
Vierkantor <vierkantor@vierkantor.com>
2014-08-10 12:21:31 +0000

    Improve FPS quite a bit by using a separate background image

--- Graphics.py
+++ Graphics.py
@@ -13,13 +13,22 @@ except ImportError:
 	pass;
 
 size = width, height = 1024, 768;
-scale = 8;
-center = (50, 0);
+
+# the width in pixels of one degree on the map
+scale = oldScale = 8;
+# center of screen
+center = oldCenter = (50, 0);
+
+# an extra buffer for speeding up background (links, nodes and places)
+background = pygame.Surface(size);
+# make sure it's drawn initially (also: old... variables)
+backgroundDirty = True;
 
 # screen is initialized when the main module runs (so things can get imported without displaying a screen)
 screen = None;
 
-# move standard parallel to about 50 degrees
+# move standard parallel to where we are looking
+# to make the equirectangular projection look a bit better
 parallelCorrection = math.cos(center[0] / 180 * math.pi);
 
 def GetPos(pos):
@@ -42,34 +51,44 @@ def GetWorldPos(pos):
 following = None;
 
 def Draw():
-	global following, center, parallelCorrection;
-	
-	# make the center of the screen look right
-	parallelCorrection = math.cos(center[0] / 180 * math.pi);
-
-	screen.fill((255, 255, 255));
+	global following, center, oldCenter, oldScale, backgroundDirty, parallelCorrection;
 	
 	if following != None:
 		center = following.pos;
 	
-	if scale > 1024:
-		for place in Data.places:
-			pos = GetPos(Data.nodes[Data.places[place]].pos);
-			if pos[0] < 0 or pos[0] > width or pos[1] < 0 or pos[1] > height:
-				continue;
-			pygame.draw.circle(screen, (0, 0, 0), pos, 3, 0);
-			text = font.render(place, 1, (0, 0, 0));
-			textpos = text.get_rect().move((pos[0], pos[1] - 12));
-			screen.blit(text, textpos);
+	# background is dirty
+	if center != oldCenter or scale != oldScale or backgroundDirty:
+		oldCenter = center;
+		oldScale = scale;
+		backgroundDirty = False;
+		
+		parallelCorrection = math.cos(center[0] / 180 * math.pi);
+		
+		# redraw background
+		background.fill((255, 255, 255));
+		
+		if scale > 1024:
+			for place in Data.places:
+				pos = GetPos(Data.nodes[Data.places[place]].pos);
+				if pos[0] < 0 or pos[0] > width or pos[1] < 0 or pos[1] > height:
+					continue;
+				pygame.draw.circle(background, (0, 0, 0), pos, 3, 0);
+				text = font.render(place, 1, (0, 0, 0));
+				textpos = text.get_rect().move((pos[0], pos[1] - 12));
+				background.blit(text, textpos);
+		
+		for node in Data.links:
+			if scale > 65536:
+				pos = GetPos(Data.nodes[node].pos);
+				if 0 <= pos[0] <= width and 0 <= pos[1] <= height:
+					Data.nodes[node].Draw(background);
+			for link in Data.links[node]:
+				Data.links[node][link].Draw(background);
 	
-	for node in Data.links:
-		if scale > 65536:
-			pos = GetPos(Data.nodes[node].pos);
-			if 0 <= pos[0] <= width and 0 <= pos[1] <= height:
-				Data.nodes[node].Draw(screen);
-		for link in Data.links[node]:
-			Data.links[node][link].Draw(screen);
+	# draw the unchanged objects
+	screen.blit(background, (0, 0));
 	
+	# draw the changed objects
 	for train in Data.trains:
 		pos = GetPos(Data.trains[train].pos);
 		if 0 <= pos[0] <= width and 0 <= pos[1] <= height:
